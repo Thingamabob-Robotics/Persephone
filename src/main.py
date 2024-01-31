f169ec177d70ede6d91869e2a962d92a800f6537
Maxine <146043018+MaxineToTheStars@users.noreply.github.com>
2024-01-31 16:09:15 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -25,30 +25,32 @@ controller: Controller = Controller(PRIMARY)
 intakeInSelection: Controller.Button = controller.buttonL1  # Top left
 intakeOutSelection: Controller.Button = controller.buttonL2  # Top left bottom
 
-moveArmUpSelection: Controller.Button = controller.buttonR1  # Top Right
-moveArmDownSelection: Controller.Button = controller.buttonR2  # Top right down
+moveArmUpSelection: Controller.Button = controller.buttonUp  # Top Right
+moveArmDownSelection: Controller.Button = controller.buttonDown  # Top right down
 
-wingToggleInSelection: Controller.Button = controller.buttonA  # A button
-wingToggleOutSelection: Controller.Button = controller.buttonB  # B Button
+# wingToggleInSelection: Controller.Button = controller.buttonA  # A button
+wingToggleSelection: Controller.Button = controller.buttonA  # B Button
 
 leftAxisSelection: Controller.Axis = controller.axis3  # Top-Bottom left
 rightAxisSelection: Controller.Axis = controller.axis2  # Top-Bottom right
 
 # Wings
-WINGS_TRIPORT_PORT: DigitalOut = DigitalOut(brain.three_wire_port.a)
+WINGS_TRIPORT_PORT: DigitalOut = DigitalOut(brain.three_wire_port.b)
+WINGS_TRIPORT_PORT1: DigitalOut = DigitalOut(brain.three_wire_port.h)
+
 
 # Intake
-INTAKE_MOTOR_PORT: int = Ports.PORT12
+INTAKE_MOTOR_PORT: int = Ports.PORT19
 
 # Arms
-LEFT_ARM_MOTOR_PORT: int = Ports.PORT9
-RIGHT_ARM_MOTOR_PORT: int = Ports.PORT2
+LEFT_ARM_MOTOR_PORT: int = Ports.PORT13
+RIGHT_ARM_MOTOR_PORT: int = Ports.PORT19
 
 # Drivetrain
-LEFT_FRONT_MOTOR_PORT: int = Ports.PORT20
-LEFT_BACK_MOTOR_PORT: int = Ports.PORT10
-RIGHT_FRONT_MOTOR_PORT: int = Ports.PORT11
-RIGHT_BACK_MOTOR_PORT: int = Ports.PORT1
+LEFT_FRONT_MOTOR_PORT: int = Ports.PORT1
+LEFT_BACK_MOTOR_PORT: int = Ports.PORT11
+RIGHT_FRONT_MOTOR_PORT: int = Ports.PORT10
+RIGHT_BACK_MOTOR_PORT: int = Ports.PORT20
 
 # Enums
 
@@ -58,18 +60,18 @@ RIGHT_BACK_MOTOR_PORT: int = Ports.PORT1
 
 # Motor definitions
 INTAKE_MOTOR: Motor = Motor(INTAKE_MOTOR_PORT, GearSetting.RATIO_18_1, False)
-LEFT_FRONT_MOTOR: Motor = Motor(LEFT_FRONT_MOTOR_PORT, GearSetting.RATIO_18_1, False)
+LEFT_FRONT_MOTOR: Motor = Motor(LEFT_FRONT_MOTOR_PORT, GearSetting.RATIO_18_1, True)
 LEFT_BACK_MOTOR: Motor = Motor(LEFT_BACK_MOTOR_PORT, GearSetting.RATIO_18_1, True)
 RIGHT_FRONT_MOTOR: Motor = Motor(RIGHT_FRONT_MOTOR_PORT, GearSetting.RATIO_18_1, False)
-RIGHT_BACK_MOTOR: Motor = Motor(RIGHT_BACK_MOTOR_PORT, GearSetting.RATIO_18_1, True)
+RIGHT_BACK_MOTOR: Motor = Motor(RIGHT_BACK_MOTOR_PORT, GearSetting.RATIO_18_1, False)
 LEFT_ARM_MOTOR: Motor = Motor(LEFT_ARM_MOTOR_PORT, GearSetting.RATIO_18_1, False)
-RIGHT_ARM_MOTOR: Motor = Motor(RIGHT_ARM_MOTOR_PORT, GearSetting.RATIO_18_1, False)
+RIGHT_ARM_MOTOR: Motor = Motor(RIGHT_ARM_MOTOR_PORT, GearSetting.RATIO_18_1, True)
 
 LEFT_DOM_MOTOR_GROUP: MotorGroup = MotorGroup(
-    LEFT_FRONT_MOTOR, RIGHT_BACK_MOTOR
+    LEFT_FRONT_MOTOR, LEFT_BACK_MOTOR
 )  # 20, 1
 RIGHT_DOM_MOTOR_GROUP: MotorGroup = MotorGroup(
-    RIGHT_FRONT_MOTOR, LEFT_BACK_MOTOR
+    RIGHT_FRONT_MOTOR, RIGHT_BACK_MOTOR
 )  # 11, 10
 
 ARM_MOTOR_GROUP: MotorGroup = MotorGroup(LEFT_ARM_MOTOR, RIGHT_ARM_MOTOR)
@@ -78,10 +80,12 @@ ARM_MOTOR_GROUP: MotorGroup = MotorGroup(LEFT_ARM_MOTOR, RIGHT_ARM_MOTOR)
 
 # Private Variables
 
+
 # main()
 def main() -> None:
     # Create a new Competition instance
     Competition(driver_control, auton_control)
+
 
 # Public Methods
 def driver_control() -> None:
@@ -99,31 +103,27 @@ def driver_control() -> None:
     # Wing Control (threaded)
     Thread(_driver_control_wing_control_thread)
 
+
 def auton_control() -> None:
-    ARM_MOTOR_GROUP.set_velocity(100, PERCENT)
-    ARM_MOTOR_GROUP.spin()
-    wait(20, SECONDS)
-    LEFT_DOM_MOTOR_GROUP.set_velocity(100, PERCENT)
-    LEFT_DOM_MOTOR_GROUP.spin()
-    wait(2, SECONDS)
-    LEFT_DOM_MOTOR_GROUP.set_velocity(100, PERCENT)
-    RIGHT_DOM_MOTOR_GROUP.set_velocity(100, PERCENT)
-    wait(5, SECONDS)
-    LEFT_DOM_MOTOR_GROUP.stop(coast)
-    wait(1, SECONDS)
-    WINGS_TRIPORT_PORT.set(1)
-    LEFT_DOM_MOTOR_GROUP.spin()
+    # Move forward and back
+    if True:
+        # Skill Auton
+        pass
+    else:
+        # Normal Auton
+        pass
+
 
 # Private Methods
 def _driver_control_movement_thread() -> None:
-    while 1:
+    while True:
         # Read joystick values
         leftAxisValue: int = leftAxisSelection.position()
         rightAxisValue: int = rightAxisSelection.position()
 
         # Set the MotorGroup velocity
-        LEFT_DOM_MOTOR_GROUP.set_velocity(leftAxisValue)
-        RIGHT_DOM_MOTOR_GROUP.set_velocity(rightAxisValue)
+        LEFT_DOM_MOTOR_GROUP.set_velocity(leftAxisValue, PERCENT)
+        RIGHT_DOM_MOTOR_GROUP.set_velocity(rightAxisValue, PERCENT)
 
         # Move
         LEFT_DOM_MOTOR_GROUP.spin(FORWARD)
@@ -132,20 +132,22 @@ def _driver_control_movement_thread() -> None:
         # Stop the Thread from killing itself
         wait(10, MSEC)
 
+
 def _driver_control_arm_control_thread() -> None:
     while 1:
         # Wait for button toggles
         if moveArmUpSelection.pressing():
-            ARM_MOTOR_GROUP.set_velocity(85, PERCENT)
+            ARM_MOTOR_GROUP.set_velocity(60, PERCENT)
             ARM_MOTOR_GROUP.spin(FORWARD)
         elif moveArmDownSelection.pressing():
-            ARM_MOTOR_GROUP.set_velocity(-85, PERCENT)
+            ARM_MOTOR_GROUP.set_velocity(-60, PERCENT)
             ARM_MOTOR_GROUP.spin(FORWARD)
         else:
             ARM_MOTOR_GROUP.stop(HOLD)
 
         # Stop the Thread from killing itself
         wait(10, MSEC)
+
 
 def _driver_control_intake_control_thread() -> None:
     while 1:
@@ -162,15 +164,20 @@ def _driver_control_intake_control_thread() -> None:
         # Stop the Thread from killing itself
         wait(10, MSEC)
 
+
 def _driver_control_wing_control_thread() -> None:
     while 1:
         # Wait for button toggles
-        if wingToggleInSelection.pressing():
-            WINGS_TRIPORT_PORT.set(0)  # Set low to retract
-        elif wingToggleOutSelection.pressing():
-            WINGS_TRIPORT_PORT.set(1)  # Set high to expand
+        if wingToggleSelection.pressing():
+            if WINGS_TRIPORT_PORT1.value():
+                WINGS_TRIPORT_PORT.set(0)
+                WINGS_TRIPORT_PORT1.set(0)
+            else:
+                WINGS_TRIPORT_PORT.set(1)
+                WINGS_TRIPORT_PORT1.set(1)
 
         wait(10, MSEC)
+
 
 # Run
 if __name__ == "__main__":
